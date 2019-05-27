523a1f3b7125782471c9aded787bb3921b60b6cc
Hafizh <hafizhsalsabilap@gmail.com>
2019-05-27 10:22:22 +0700

    Set-Get Cust

---
+++ src/bioskop/Customer.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bioskop;
+
+/**
+ *
+ * @author samsung
+ */
+public class Customer {
+    private static String CustomerName;
+    private static String CustomerPhone;
+
+    /**
+     * @return the CustomerName
+     */
+    public String getCustomerName() {
+        return CustomerName;
+    }
+
+    /**
+     * @param n
+     * @param CustomerName the CustomerName to set
+     */
+    public void setCustomerName(String n) {
+        this.CustomerName = n;
+    }
+
+    /**
+     * @return the CustomerPhone
+     */
+    public String getCustomerPhone() {
+        return CustomerPhone;
+    }
+
+    /**
+     * @param t
+     * @param CustomerPhone the CustomerPhone to set
+     */
+    public void setCustomerPhone(String t) {
+        this.CustomerPhone = t;
+    }
+}

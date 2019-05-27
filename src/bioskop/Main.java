dc8cec1f7ecf47bb227e6058fd89bc9918d90e10
irvannaufal <>
2019-05-27 10:02:15 +0700

    enak

--- src/bioskop/Main.java
+++ src/bioskop/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("Test GITHUB Bosku");
         System.out.println("Cobak gak ?");
         System.out.println("enak mas..");
+        System.out.println("aku subur");
     }
     
 }

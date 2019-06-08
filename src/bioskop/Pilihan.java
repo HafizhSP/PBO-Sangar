5b3671cc10a49d2b18a46044e11c842a69758655
irvannaufal <irvannaufal15@gmail.com>
2019-06-08 09:12:27 +0700

    tambahanjam

--- src/bioskop/Pilihan.java
+++ src/bioskop/Pilihan.java
@@ -20,6 +20,8 @@ public class Pilihan extends Biodata{
     private int pilihanjam;
     int harga;
     private int judulFilm;
+    private Object pilihJam;
+    private int pilihjam;
    
     public void biodata(){
         bio();
@@ -58,16 +60,16 @@ public class Pilihan extends Biodata{
         }
     
     public void jam() {
-        String pilihJam = "[1]. 12.30\n"
+        String pilihanam = "[1]. 12.30\n"
                 + "[2]. 18.00\n"
                 + "[3]. 20.00\n";
-//        System.out.println("Pilihan Jam Hari ini :");
-//        System.out.println("[1]. 12.30");
-//        System.out.println("[2]. 18.00");
-//        System.out.println("[3]. 20.00");
-//        System.out.print("Masukan Pilihan :");
+        System.out.println("Pilihan Jam Hari ini :");
+        System.out.println("[1]. 12.30");
+        System.out.println("[2]. 18.00");
+        System.out.println("[3]. 20.00");
+        System.out.print("Masukan Pilihan :");
         try {
-            pilihanjam = Integer.parseInt(JOptionPane.showInputDialog(null, pilihJam, "Pilihan JAM Hari Ini", JOptionPane.QUESTION_MESSAGE));
+            pilihjam = Integer.parseInt(JOptionPane.showInputDialog(null, pilihJam, "Pilihan JAM Hari Ini", JOptionPane.QUESTION_MESSAGE));
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Tekan OKE untuk keluar program");
             System.exit(0);

7f3601f5413d82b2db02394251816734542e70a8
Miftakhul Hafidzun Nafi <miftakhulhafidzun@users.noreply.github.com>
2019-06-09 18:27:37 +0700

    Controller Program

---
+++ src/bioskop/Controller.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bioskop;
+
+/**
+ *
+ * @author Miftakhul Hafidz
+ */
+public class Controller {
+
+
+    public void Control() {
+        Pilihan bio = new Pilihan();
+        Bayar byr = new Bayar();
+        while (true) {
+            bio.bio();
+            bio.film();
+            bio.jam();
+            bio.totalOrang();
+            bio.kursi();
+        
+        
+        while (true){
+                byr.cetak();
+                byr.Bayar();
+            }
+        }    
+        
+
+    }
+}

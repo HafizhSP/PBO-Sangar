d24407076a0de019d862adf376563ed05c7a3d8b
YannuarSalim <Yannuarsalim1@gmail.com>
2019-06-09 09:49:23 +0700

    edit class pilihan , method jam

--- src/bioskop/Pilihan.java
+++ src/bioskop/Pilihan.java
@@ -60,12 +60,9 @@ public class Pilihan extends Biodata{
     }
     
     public void jam(){
-        String pilihJam = "[1]. 12.30\n" + "[2]. 18.00\n" + "[3]. 20.00\n";
-        System.out.println("Pilihan Jam Hari ini :");
-        System.out.println("[1]. 12.30");
-        System.out.println("[2]. 18.00");
-        System.out.println("[3]. 20.00");
-        System.out.print("Masukan Pilihan :");
+        String pilihJam = "[1]. 12.30\n"
+                + "[2]. 18.00\n"
+                + "[3]. 20.00\n";
         try {
             pilihanjam = Integer.parseInt(JOptionPane.showInputDialog(null, pilihJam, "Pilihan JAM Hari Ini", JOptionPane.QUESTION_MESSAGE));
         } catch (Exception e) {
